416ee38ed73724f8dbdf3905357272f16f02615d
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-10-05 09:26:42 +0200

    first commit

---
+++ test.py
@@ -0,0 +1,63 @@
+
+class Konto:
+    def __init__(self):
+        self.kontonr = ""
+        self.saldo = 0
+        self.pinkod = ""
+
+
+listaMedKonton = []
+while True:
+    print("1. Skapa konto")
+    print("2. Visa alla konton")
+    sel = input("Selection:")
+    if sel == "1":
+        knr = input("Ange kontonummer:")
+        pin = input("Ange pinkod:")
+        k = Konto()
+        k.kontonr = knr
+        k.pinkod = pin
+        listaMedKonton.append(k)
+    if sel == "2":
+        for k in listaMedKonton:
+            print(f"{k.kontonr} {k.saldo}")        
+
+
+
+# MIXED RESPONSIBILITIES
+
+# SRP 
+
+def calculateSalary(age, hoursWorked, hourlySalary):
+    result = 0
+    if age > 48:
+        result = hoursWorked * hourlySalary
+        if hoursWorked > 10:
+            result = result * 1.10 + 2000
+    else:
+        result = hoursWorked * hourlySalary
+        if age < 21:
+            result = result * 0.9 - 10
+            if result < 0:
+                result = 0
+    return result
+    #print(f"Lön:{result}")
+
+
+while True:
+
+    age = int(input("Enter age:"))
+    hoursWorked = int(input("Enter hours:"))
+    hoursSalary = int(input("Enter hourly salary:"))
+    result = calculateSalary(age,hoursWorked,hoursSalary)
+
+
+
+    age = int(input("Ange din ålder:"))
+    hoursWorked = int(input("Hur många timmar:"))
+    hoursSalary = int(input("Ange din timlön:"))
+    result = calculateSalary(age,hoursWorked,hoursSalary)
+    print(f"Lön:{result}")
+
+    
+
